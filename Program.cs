837be7eeb7d34ad1bd82a2c42c141ed1cb9485d7
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-06-22 22:56:01 -0300

    Projeto Final

--- Program.cs
+++ Program.cs
@@ -50,6 +50,3 @@ for (int i = 0; i < cidadesPercurso.Count - 1; i++)
 Console.WriteLine("\nA soma total das distâncias é de {0} km", soma);
 
 Console.ReadKey();
-Console.Clear();
-
-return;
--- obj/Release/net9.0/DistanciaPercorrida.AssemblyInfo.cs
+++ obj/Release/net9.0/DistanciaPercorrida.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DistanciaPercorrida")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da161dbc406c6733a9a88b31f66a1921ce9f680b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d54780399e9f507367dde37493984402252426b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DistanciaPercorrida")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DistanciaPercorrida")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
